0f9e26be6ecadd2ddaeeb6e86c0a96819e23938a
fedev2024 <fe.dev.ba2024@gmail.com>
2026-06-08 10:32:07 +0530

    ttt

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,7 +1,5 @@
 'use client'
-import Image from 'next/image'
-import logo from '../public/uploads/pictures/logo.png'
-import Icon from '../public/uploads/pictures/site icon.png'
+
 import Link from 'next/link'
 import { useState } from 'react'
 import { useRouter } from 'next/navigation'
